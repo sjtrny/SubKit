7877c88511dd6abcf26cc40d22be6eb28881f66a
Stephen Tierney <sjtrny@gmail.com>
2016-02-23 01:00:30 +1100

    Fixed header

--- common/solve_l2.m
+++ common/solve_l2.m
@@ -1,7 +1,7 @@
 function [ x ] = solve_l2( w, lambda )
 %% Solves the following
 %
-%   arg min_{x} || x - b ||_2^2 + lambda/2 || x ||_2^2
+%   arg min_{x} 1/2 || x - b ||_2^2 + lambda/2 || x ||_2^2
 %
 % Created by Stephen Tierney
 % stierney@csu.edu.au
